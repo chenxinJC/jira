1113ebbd2e89b7de5badcca578890dc7335c98b9
chenxin <517242860@qq.com>
2021-08-03 13:53:37 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <span>11111111</span><span>222222222</span>
-    </div>
+    <div className="App"><span>11111111</span><span>222222222</span></div>
   );
 }
 
