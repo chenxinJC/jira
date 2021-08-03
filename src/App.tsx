2eb1698eb4911bcd82e3b27a79ed29df126f47c5
chenxin <517242860@qq.com>
2021-08-03 13:56:10 +0800

    app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App"><span>11111111</span><span>222222222</span></div>
-  );
+  
+  return (<div className="App"><span>11111111</span><span>222222222</span></div>);
 }
 
 export default App;
