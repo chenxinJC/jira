782a6c35a0851e6068ed0bd76c0f2f4fc47b8191
chenxin <517242860@qq.com>
2021-08-03 16:43:38 +0800

    upd: app

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <span>11111111</span>
-      <span>222222222</span>33333<div>444</div>5555577<div>1</div>22333334511
     </div>
   );
 }
-
-function test() {
-  let a = "1";
-  if (a) {
-    console.log(1);
-  } else {
-    console.log(2);
-  }
-}
-
 export default App;
